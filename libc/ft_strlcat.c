e2d5c823107943dd8674b2811c15b4eea1ca7a9c
sepabeat <sepabeat@gmail.com>
2023-04-20 17:22:21 +0200

    ultima subida jueves

--- libc/ft_strlcat.c
+++ libc/ft_strlcat.c
@@ -1,45 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/20 13:36:13 by salperez          #+#    #+#             */
-/*   Updated: 2023/04/20 16:06:23 by salperez         ###   ########.fr       */
+/*   Created: 2023/04/20 15:40:27 by salperez          #+#    #+#             */
+/*   Updated: 2023/04/20 15:57:30 by salperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-size_t	ft_strlcpy(char	*dst, const char	*src, size_t	dstsize)
-{
-	size_t	i;
+// size_t	ft_strlcat(char	*dst, const char	*src, size_t	dstsize)
+// {
 
-	i = 0;
-	while (src[i] != '\0' && i < dstsize -1)
-	{
-		dst[i] = src [i];
-		i++;
-	}
-	dst[i] = '\0';
-	while (src[i] != '\0')
-		i++;
-	return (i);
-}
+	
+// }
 
 int main (void)
 {
-	char	dst[] = "chupachups";
-	char	src[] = "upas";
+	char	dst[] = "hola";
+	char	src[] = "cabesa";
 	size_t	dstsize = 4;
-	
+
 	printf("%s\n", dst);
 	printf("%s\n", src);
-	printf("%zu\n", ft_strlcpy(dst, src, dstsize));
-	printf("%lu\n", strlcpy(dst, src, dstsize));
+	// printf("%d\n", ft_strlcat(dst, src, dstsize));
+	printf("%lu\n", strlcat(dst, src, 4));
 	printf("%s\n", dst);
 	printf("%s\n", src);
-	return (0);;
+	return (0);
 }
