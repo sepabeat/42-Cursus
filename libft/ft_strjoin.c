a3e47564e6bdd5621578f7d7d9a5a94abbf6a437
sepabeat <sepabeat@gmail.com>
2023-04-25 18:27:39 +0200

    falta verificar paco strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 17:55:33 by salperez          #+#    #+#             */
+/*   Updated: 2023/04/25 18:27:26 by salperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const	*s1, char const	*s2)
+{
+	char			*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+	free(str);
+}
+
+int main (void)
+{
+	char s1[] = "hola";
+	char s2[] = "mundo";
+	printf("%s", ft_strjoin(s1, s2));
+}
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,12 +6,48 @@
 /*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:15:24 by salperez          #+#    #+#             */
-/*   Updated: 2023/04/25 17:04:10 by salperez         ###   ########.fr       */
+/*   Updated: 2023/04/25 17:51:25 by salperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
+
+// void	ft_bzero(void *s, size_t n)
+// {
+// 	unsigned int	i;
+// 	unsigned char	*f;
+
+// 	f = s;
+// 	i = 0;
+// 	while (i < n)
+// 	{
+// 		*f = '\0';
+// 		i++;
+// 		f++;
+// 	}
+// }
+
+// void	*ft_calloc(size_t count, size_t size)
+// {
+// 	int	*ptr;
+
+// 	ptr = malloc(count * size);
+// 	if (!ptr)
+// 		return (0);
+// 	ft_bzero(ptr, count * size);
+// 	return (ptr);
+// }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -36,5 +72,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 // int	main(void)
 // {
+// 	printf("%s", ft_substr("hola", 1, 1));
 // 	return (0);
 // }
