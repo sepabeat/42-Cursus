aad62d3824b43f790095741576e9319fa6b0bb1c
sepabeat <sepabeat@gmail.com>
2023-04-26 17:25:19 +0200

    strtrim corregido, repasarlo

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,88 +6,62 @@
 /*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 11:30:37 by salperez          #+#    #+#             */
-/*   Updated: 2023/04/26 15:40:06 by salperez         ###   ########.fr       */
+/*   Updated: 2023/04/26 17:24:50 by salperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-size_t	ft_strlen(const char *s)
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
+
+int	ft_check_set(char c, char const *set)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-int	ft_check_start(char const	*s1, char const	*set)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (s1[i] != '\0')
+	while (*set)
 	{
-	j = 0;
-		while (s1[i] != set[j])
-		{
-			if (set[j] == '\0')
-				return (i);
-			j++;
-		}
-		i++;
+		if (c == *set)
+			return (1);
+		set++;
 	}
-	return (i);
+	return (0);
 }
 
-int	ft_check_end(char const	*s1, char const	*set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
-	size_t	j;
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
 
-	len = ft_strlen(s1) - 1;
-	while (len > 0)
-	{
-	j = 0;
-		while (s1[len] != set[j])
-		{
-			if (set[j] == '\0')
-				return (len);
-			j++;
-		}
-		len--;
-	}
-	return (ft_strlen(s1) - 1);
-}
-
-
-/* char	*ft_strtrim(char const	*s1, char const	*set)
-{
-	char			*str;
-	size_t			i;
-	size_t			j;
-	size_t			x;
-
-	i = 0;
-	j = 0;
-	x = 0;
-	str = (char *)malloc((ft_strlen(s1) - ft_strlen(set) + 1) * sizeof(char));
-	if (str == NULL)
+	if (!s1 || !set)
 		return (NULL);
-	
+	start = 0;
+	while (s1[start] && ft_check_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_check_set(s1[end - 1], set))
+		end--;
+	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
 	str[i] = '\0';
 	return (str);
-	free(str);
-} */
-
-int main (void)
-{
-	char s1[] = "hola k ase";
-	char set[] = "oh";
-	// printf("%s", ft_strtrim(s1, set));
-	printf("%d\n", ft_check_start(s1, set));
-	printf("%d", ft_check_end(s1, set));
 }
+
+// int main (void)
+// {
+// 	char s1[] = "lahola malargala";
+// 	char set[] = "la";
+// 	printf("%s", ft_strtrim(s1, set));
+// }
