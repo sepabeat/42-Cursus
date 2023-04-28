762b982b35ba85f3505a93ffd610db67614d5c26
sepabeat <sepabeat@gmail.com>
2023-04-28 16:53:25 +0200

    strmapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 15:19:11 by salperez          #+#    #+#             */
+/*   Updated: 2023/04/28 16:53:16 by salperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strmapi(char const	*s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = (char *) malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+// int main (void)
+// {
+// 	char (*f)(unsigned int, char);
+// 	f = ft_toupper;
+// 	// Esta es la sintaxis de igualar f desde el main con una función
+// 	// existente
+// }
+/* strmapi se utiliza para crear una especie de función vacía
+que nos permite llamar a otras funciones, es como una función comodín
+*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 12:51:31 by salperez          #+#    #+#             */
-/*   Updated: 2023/04/28 14:57:42 by salperez         ###   ########.fr       */
+/*   Updated: 2023/04/28 16:51:35 by salperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const	*s, char (*f)(unsigned int, char));
 
 #endif
