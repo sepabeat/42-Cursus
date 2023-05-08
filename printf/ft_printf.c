9776b83b49178300f78b09dc3b9d700da4cadb7e
sepabeat <sepabeat@gmail.com>
2023-05-08 19:19:37 +0200

    printf begins

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,25 +6,54 @@
 /*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 12:58:21 by salperez          #+#    #+#             */
-/*   Updated: 2023/05/08 18:25:27 by salperez         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:19:18 by salperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
+#include "ft_printf.h"
 
 int	ft_printf(char const *str, ...)
 {
-	int		i;
+	size_t	i;
 	va_list	argptr;
 
-	va_start(argptr, i);
+	va_start(argptr, str);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if ((str[i] == 'd') || (str[i] == 'i'))
+				ft_putnbri(va_arg(argptr, int), i);
+			else if (str[i] == 's')
+				printf("%s", va_arg(argptr, char *));
+			else if (str[i] == 'c')
+				printf("%c", va_arg(argptr, int));
+			else if (str[i] == 'x')
+				printf("%x", va_arg(argptr, unsigned int));
+			else if (str[i] == 'X')
+				printf("%X", va_arg(argptr, unsigned int));
+			else if (str[i] == 'p')
+				printf("%p", va_arg(argptr, void *));
+			else if (str[i] == 'u')
+				printf("%u", va_arg(argptr, unsigned int));
+			else if (str[i] == '%')
+				printf("%%");
+		}
+		else
+			printf("%c", str[i]);
+		i++;
+	}
+	va_end(argptr);
+	return (i);
 }
-
 
 int	main(void)
 {
 	printf("check, check %d, hola\n", 25);
-/* 	ft_printf("check, check %d, hola\n", 25);
+	/* 	ft_printf("check, check %d, hola\n", 25);
  */
 }
---
+++ printf/ft_printf_functions.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_functions.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 19:16:29 by salperez          #+#    #+#             */
+/*   Updated: 2023/05/08 19:18:37 by salperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+size_t	ft_strlen(char const *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
