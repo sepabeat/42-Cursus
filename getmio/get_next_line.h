9e94ec779649b6cc8fa4bae14b12f3dbc19838c8
sepabeat <sepabeat@gmail.com>
2023-05-25 17:58:29 +0200

    testing my own gnl

---
+++ getmio/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salperez <salperez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 12:15:11 by salperez          #+#    #+#             */
+/*   Updated: 2023/05/25 17:57:50 by salperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+
+# endif
+
+# include <fcntl.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/types.h>
+
+char *get_next_line(int fd);
+char	*ft_strjoin(char const	*s1, char const	*s2);
+char	*ft_strchr(const char *s, int c);
+
+#endif
---
+++ getmio/get_next_line_utils.c
@@ -0,0 +1,42 @@
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const	*s1, char const	*s2)
+{
+	char			*str;
+	size_t			i;
+	size_t			j;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+	free(str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != (char)c)
+	{
+		if (s[i] == '\0')
+			return (NULL);
+		i++;
+	}
+	return ((char *)&s[i]);
+}
